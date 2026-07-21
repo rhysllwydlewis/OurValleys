813e2cdebc2123a7cca3c3dd6e6dc6e8812aba1d
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-07-21 21:09:38 +0100

    Export business governance schema

--- src/lib/database/schema/index.ts
+++ src/lib/database/schema/index.ts
@@ -1,5 +1,6 @@
 export * from "./auth";
 export * from "./business";
+export * from "./business-governance";
 export * from "./business-operations";
 export * from "./moderation";
 export * from "./onboarding";
