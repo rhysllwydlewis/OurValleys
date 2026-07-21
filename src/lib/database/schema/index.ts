32ac447c696c7e6843cecc60fa21608cfe1de78c
rhysllwydlewis <rhysllwydlewis@gmail.com>
2026-07-21 20:58:08 +0100

    Export business operations schema

--- src/lib/database/schema/index.ts
+++ src/lib/database/schema/index.ts
@@ -1,5 +1,6 @@
 export * from "./auth";
 export * from "./business";
+export * from "./business-operations";
 export * from "./moderation";
 export * from "./onboarding";
 export * from "./reference";
